ce6352b1ce1778dd088b118260e4b963a857a832
Manuel Pena <penamanuel628@gmail.com>
2023-06-28 17:46:07 -0300

    Validacion form

--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -1,11 +1,16 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
+const checkFields = require ("../middlewares/validateFields");
 
 const MailController = require("../controllers/mail.controller");
 const validateJwt = require("../middlewares/jwtValidator")
 
 const router = Router();
 
-router.post('/create', MailController.createMail)
+router.post('/create',[check('name').not().isEmpty(),
+                        check('email').not().isEmpty(),
+                        check('telefono').not().isEmpty(), checkFields],
+                        MailController.createMail)
 router.get('/getMails', validateJwt , MailController.getMails)
 
 
